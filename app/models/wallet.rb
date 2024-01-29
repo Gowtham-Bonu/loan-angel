64f76d4961c47e69c4a8f8fa6b2c1fd17b01e4c1
Gowtham-Bonu <190303107015@paruluniversity.ac.in>
2024-01-29 16:14:00 +0530

    ref

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,6 +1,6 @@
 class Wallet < ApplicationRecord
   belongs_to :accountable, polymorphic: true
-  
+
   validates :balance, presence: true
   validates :balance, numericality: { only_integer: true }
 end
